b78a3f69d696af82b5e5ec81dd1a0f16a154124e
Alex Studer <alex@studer.dev>
2022-09-08 12:54:48 -0400

    enough code to run a single job and then stop

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"io/fs"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/thatoddmailbox/jobmgr/config"
 	"github.com/thatoddmailbox/jobmgr/data"
@@ -18,5 +22,78 @@ func main() {
 	err = data.Init()
 	if err != nil {
 		panic(err)
+	}
+
+	for {
+		job, err := data.GetNextJobInQueue()
+		if err != nil {
+			panic(err)
+		}
+
+		log.Printf("%+v", job)
+
+		err = data.MarkJobStarted(job)
+		if err != nil {
+			panic(err)
+		}
+
+		result, tempDir, err := runJob(job)
+		if err != nil {
+			// job failed!
+			// TODO: handle this
+			panic(err)
+		}
+
+		// TODO: save artifacts
+		artifactsDir := filepath.Join(tempDir, artifactsDirName)
+		err = filepath.WalkDir(artifactsDir, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				// bubble up any errors
+				return err
+			}
+
+			if d.IsDir() {
+				return nil
+			}
+
+			relativePath := strings.Replace(path, artifactsDir, "", 1)[1:]
+
+			log.Println(relativePath)
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		tx, err := data.DB.Begin()
+		if err != nil {
+			panic(err)
+		}
+
+		err = data.InsertResult(tx, job, result)
+		if err != nil {
+			tx.Rollback()
+			panic(err)
+		}
+
+		err = data.MarkJobCompleted(tx, job)
+		if err != nil {
+			tx.Rollback()
+			panic(err)
+		}
+
+		err = tx.Commit()
+		if err != nil {
+			tx.Rollback()
+			panic(err)
+		}
+
+		// clean up the temp directory
+		err = os.RemoveAll(tempDir)
+		if err != nil {
+			panic(err)
+		}
+
+		break
 	}
 }
